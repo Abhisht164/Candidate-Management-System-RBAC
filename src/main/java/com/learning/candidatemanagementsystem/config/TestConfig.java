e92691441e816ed50244da27fa8b191af1aac313
Abhisht164 <abhishtpratapsingh@gmail.com>
2025-07-25 17:08:16 +0530

    test 6

--- src/main/java/com/learning/candidatemanagementsystem/config/TestConfig.java
+++ src/main/java/com/learning/candidatemanagementsystem/config/TestConfig.java
@@ -5,4 +5,6 @@ public class TestConfig {
     static double MAX_LONGITUDE = 180;
     static double MIN_LATITUDE = -90;
     static double MIN_LONGITUDE = -180;
+    static double LATITUDE_STEP = 0.0001;
+    static double LONGITUDE_STEP = 0.0001;
 }
