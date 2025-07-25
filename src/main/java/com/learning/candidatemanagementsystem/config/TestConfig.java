ca699e7e9cd8fb8f1058a7f897d0c42bd61cfa26
Abhisht Pratap Singh <abhishtpratapsingh@gmail.com>
2025-07-25 17:20:04 +0530

    Delete src/main/java/com/learning/candidatemanagementsystem/config/TestConfig.java

--- src/main/java/com/learning/candidatemanagementsystem/config/TestConfig.java
+++
@@ -1,8 +0,0 @@
-package com.learning.candidatemanagementsystem.config;
-
-public class TestConfig {
-    static double MAX_LATITUDE = 90;
-    static double MAX_LONGITUDE = 180;
-    static double MIN_LATITUDE = -90;
-    static double MIN_LONGITUDE = -180;
-}
